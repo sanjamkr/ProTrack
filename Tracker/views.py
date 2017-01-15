34557fef1a935aa850d1e9fc64184f290f705b73
Bhavya Garg <bhavyagarg12@gmail.com>
2017-01-15 17:41:10 +0530

    gitignore added

--- Tracker/views.py
+++ Tracker/views.py
@@ -4,14 +4,14 @@ from django.http import HttpResponse
 
 from .models import task
 
-def index (request, task_id):
+def index (request):
     # tasks = task.objects.order_by('-due_date')[:5]
     # context = {'task_list': tasks}
     # return render(request,'task/index.html',context)
-	return HttpResponse("You're looking at task %s." % task_id)
+    return HttpResponse("You're in task view")
     
 def detail(request,task_id):
-    task = get_object_or_404(task,pk=task_id)
+    #task = get_object_or_404(task,pk=task_id)
     return HttpResponse("You're looking at task %s." % task_id)
     #return render(request,'task/detail.html',{'task': task})
 
@@ -20,6 +20,6 @@ def tag(request,task_id):
     return HttpResponse(response % task_id)
 
 def comment(request,task_id):
-    response = "comments %s."
+    response = "comment %s."
     return HttpResponse(response % task_id)
 
