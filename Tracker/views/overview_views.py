e906c1d4a17cc93a8e09bc4414574d4102f892d7
Bhavya Garg <bhavyagarg12@gmail.com>
2017-01-22 15:20:58 +0530

    Specific Related content among diferent models rendered in views

--- Tracker/views/overview_views.py
+++ Tracker/views/overview_views.py
@@ -17,20 +17,14 @@ def homepage(request):
 
 def group_detail(request,group_id):
     gname = get_object_or_404(group,pk=group_id)
-    project_list = project.objects.order_by('id')[:]
     context = {
-        'project_list' : project_list,
         'group': gname,
     }
     return render(request,'Tracker/group_detail.html',context)
 
 def project_detail(request,project_id):
     p = get_object_or_404(project,pk=project_id)
-    tl = task.objects.order_by('-due_date')[:]
-    sl = sprint.objects.order_by('id')[:]
     context = {
         'project' : p,
-        'task_list': tl,
-        'sprint_list':sl,
     }
     return render(request,'Tracker/project_detail.html',context)
