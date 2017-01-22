f3c74b3f94e83f76467c9990eb6b9ad2c66adafa
Bhavya Garg <bhavyagarg12@gmail.com>
2017-01-22 16:49:22 +0530

    URL's simplified, User-Entered-Data accessed in Sprint_Views

--- Tracker/urls.py
+++ Tracker/urls.py
@@ -4,11 +4,12 @@ from . import views
 #app_name = 'task'
 urlpatterns = [
     url(r'^$', views.login, name='login'), #1st login screen
-    url(r'^MemberId=[0-9]+/$', views.homepage, name='homepage'), #Member page, shows list of groups
-    url(r'^MemberId=[0-9]+/GroupId=(?P<group_id>[0-9]+)/$', views.group_detail, name='group_detail'), #Group details  #Includes list of projects
-    url(r'^MemberId=[0-9]+/GroupId=[0-9]+/ProjectId=(?P<project_id>[0-9]+)/$', views.project_detail, name='project_detail'), #Project details,includes list of tasks & sprints
-    url(r'^MemberId=[0-9]+/GroupId=[0-9]+/ProjectId=[0-9]+/TaskId=(?P<task_id>[0-9]+)/$', views.task_detail,name= 'task_detail'),
-    url(r'^MemberId=[0-9]+/GroupId=[0-9]+/ProjectId=[0-9]+/SprintId=(?P<sprint_id>[0-9]+)/$', views.sprint_detail, name='sprint_detail'),
+    url(r'^homepage/$', views.homepage, name='homepage'), #Member page, shows list of groups
+    url(r'^GroupId=(?P<group_id>[0-9]+)/$', views.group_detail, name='group_detail'), #Group details  #Includes list of projects
+    url(r'^ProjectId=(?P<project_id>[0-9]+)/$', views.project_detail, name='project_detail'), #Project details,includes list of tasks & sprints
+    url(r'^TaskId=(?P<task_id>[0-9]+)/$', views.task_detail,name= 'task_detail'),
+    url(r'^SprintId=(?P<sprint_id>[0-9]+)/$', views.sprint_detail, name='sprint_detail'),
 
     url(r'^task/add_task/$',views.add_task, name='add_task'),
+    url(r'^sprint/add_sprint/$',views.add_sprint, name='add_sprint'),
     ]
--- Tracker/views/sprint_views.py
+++ Tracker/views/sprint_views.py
@@ -3,6 +3,13 @@ from django.http import Http404,HttpResponse
 
 from Tracker.models import sprint
 
+def add_sprint(request):
+    return render(request,'Tracker/add_sprint.html')
+
+def added(request):
+    new_sprint_name = request.POST['sname']
+    return HttpResponse("New Sprint Name : %s" % new_sprint_name)
+
 def sprint_detail(request,sprint_id):
     return HttpResponse("You're looking at sprint %s." % sprint_id)
 
