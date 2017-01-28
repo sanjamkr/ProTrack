777e5faf6f750cc68ce460c3c8275572cec558e4
Bhavya Garg <bhavyagarg12@gmail.com>
2017-01-29 00:25:38 +0530

    detail view urls removed

--- Tracker/urls.py
+++ Tracker/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.login, name='login'), #1st login screen
     url(r'^homepage/$', views.homepage, name='homepage'), #Member page, shows list of groups
-
+    
     url(r'^add_group/$',views.add_group, name='add_group'), 
     url(r'^add_project/$',views.add_project, name='add_project'),
     url(r'^add_task/$',views.add_task, name='add_task'),
