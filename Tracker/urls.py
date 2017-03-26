ec6949a8908ff5a9816fd9ab077f65db1fe0f9f0
Sanjam <sanjamkr4@gmail.com>
2017-03-26 21:49:22 +0530

    CSS Added to Sprint View

--- Tracker/urls.py
+++ Tracker/urls.py
@@ -39,7 +39,6 @@ urlpatterns = [
     url(r'^add_tag/(?P<task_id>[0-9]+)/$',views.add_tag, name='add_tag'),
     #Sprint
     url(r'^edit_sprint/(?P<sprint_id>[0-9]+)/$',views.edit_sprint, name='edit_sprint'),
-    url(r'^spchart/(?P<sprint_id>[0-9]+)/$',views.sprintchart, name='sprintchart'),
     url(r'^delete_sprint/(?P<sprint_id>[0-9]+)/$',views.delete_sprint, name='delete_sprint'),
     #Image
 	url(r'^$', ProfileImageView.as_view(), name='home'),
