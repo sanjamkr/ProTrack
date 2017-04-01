a24c25d87f58b55a2f62f1381d3fd42ec1656714
Sanjam <sanjamkr4@gmail.com>
2017-04-01 18:45:48 +0530

    Notification Count

--- Tracker/custom_context.py
+++ Tracker/custom_context.py
@@ -1,20 +1,23 @@
 from django.contrib.auth.models import User, Group
+from django.shortcuts import render,get_object_or_404
 from django.template import RequestContext, Template
 from Tracker.models import notification
 from django.db.models import Q
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 
 def noti_count(request):
-    user = User.objects.get(username=request.user.username)
-    g = user.groups.all()[0]
-    is_member = Q(member = user)
-    is_group = Q(membergroup = g)
-    is_not_othermember = Q(othermember = user.username)
-    is_unread = Q(read = False)
+    noti_count = 0
+    if request.user.is_authenticated:
+        try:
+            user = User.objects.get(username=request.user.username)
+            g = user.groups.all()[0]
+            is_member = Q(member = user)
+            is_group = Q(membergroup = g)
+            is_not_othermember = Q(othermember = user.username)
+            is_unread = Q(read = False)
     
-    unread_notis = notification.objects.filter(is_unread & (is_member | (is_group & ~(is_not_othermember)))).order_by('-noti_create')
-    noti_count = unread_notis.count()
-
-    
-
+            unread_notis = notification.objects.filter(is_unread & (is_member | (is_group & ~(is_not_othermember)))).order_by('-noti_create')
+            noti_count = unread_notis.count()    
+        except DoesNotExist:
+            pass
     return{'noti_count': noti_count}
